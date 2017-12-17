69eede0e3b434aa41ee35dde0db99ce6a689686e
simonaoliver <sao547@raijin2.(none)>
2017-12-17 13:29:11 +1100

    increase walltime and add do qsub routine

---
+++ do_qsub.sh
@@ -0,0 +1,2 @@
+for i in `find /g/data/v10/AGDCv2/indexed_datasets/ledaps_lasrc/opendatacubepipelines.ledapslasrc/qsub_scripts -name "*.qsub"`; do qsub $i; done
+
--- ledaps_lasrc_prepare.sh
+++ ledaps_lasrc_prepare.sh
@@ -2,7 +2,7 @@
 #
 #PBS -P v10
 #PBS -q normal
-#PBS -l walltime=2:00:00,ncpus=8,mem=64GB
+#PBS -l walltime=48:00:00,ncpus=1,mem=8GB
 #PBS -l wd
 #PBS -me
 
@@ -12,4 +12,7 @@ module load gaip/dev-sen2redo
 #module load agdc-py3-prod/1.5.1
 module load parallel
 
-find /g/data/v10/projects/ARD_interoperability/L2/TARGET -name *tar.gz | parallel --jobs 8 "python ls_usgs_l2_prepare.py {} --output /g/data/v10/AGDCv2/indexed_datasets/ledaps_lasrc/opendatacubepipelines.ledapslasrc/yamls/TARGET --no-checksum --date 1/1/1999"
+HOME=/g/data/v10/AGDCv2/indexed_datasets/ledaps_lasrc/opendatacubepipelines.ledapslasrc
+DATA=/g/data/v10/projects/ARD_interoperability/L2
+
+python $HOME/ls_usgs_l2_prepare.py $DATA/TARGET --output $HOME/yamls/TARGET --no-checksum --date 1/1/1999
